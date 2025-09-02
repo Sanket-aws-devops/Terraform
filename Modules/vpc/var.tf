bb26b1ac27f2216d94d15fb24a1c5662d4daaf86
Sanket-aws-devops <157350422+Sanket-aws-devops@users.noreply.github.com>
2025-09-02 14:27:52 +0530

    Create var.tf

---
+++ Modules/vpc/var.tf
@@ -0,0 +1,14 @@
+variable "aws_vpc_cidr" {
+    description = "aws_vpc_cidr"
+#    default = "10.0.0.0/24"
+}
+
+variable "public_subnet_cidr" {
+    description = "public_subnet_cidr"
+#    default = "10.0.0.0/25"
+}
+
+variable "allowed_http_cidr" {
+    description = "allowed_http_cidr"
+#    default = "0.0.0.0/0"
+}
