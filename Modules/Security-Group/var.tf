684ff6ad1803fbf762ac7a4751851e4a233b8513
Sanket-aws-devops <157350422+Sanket-aws-devops@users.noreply.github.com>
2025-09-02 14:32:47 +0530

    Create var.tf

---
+++ Modules/Security-Group/var.tf
@@ -0,0 +1,12 @@
+variable "allowed_http_cidr" {
+    description = "aws_vpc_cidr"
+}
+
+variable "allowed_ssh_cidr" {
+    description = "allowed_ssh_cidr"
+
+}
+
+variable "vpc_id" {
+    description = "vpc_id"
+}
