632bdeb7b6883b26d1cfe10fc739333c668b883c
Sanket-aws-devops <157350422+Sanket-aws-devops@users.noreply.github.com>
2025-09-02 14:34:19 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,7 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "aws_vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "allowed_http_cidr" {}
+#variable "vpc_name" {}
+#variable "vpc_id" {}
